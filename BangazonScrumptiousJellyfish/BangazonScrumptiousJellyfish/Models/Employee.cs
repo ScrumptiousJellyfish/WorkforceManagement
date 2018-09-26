52b462c6c7f0bda1d0be8af3f3154d5cae1e591c
Jenn Lawson <jenniferlawson000@gmail.com>
2018-09-26 15:42:49 -0500

    employee model

---
+++ BangazonScrumptiousJellyfish/BangazonScrumptiousJellyfish/Models/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonScrumptiousJellyfish.Models
+{
+    public class Employee
+    {
+        
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        public bool IsSupervisor { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+
+        [Display(Name = "Employee Name")]
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+        public int DepartmentId { get; set; }
+
+        public Department Department { get; set; }
+
+        public Computer Computer { get; set; }
+    }
+}
