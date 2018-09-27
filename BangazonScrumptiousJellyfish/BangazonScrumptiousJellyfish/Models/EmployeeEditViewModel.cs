ae9fa4ec05875933d524d2828b1bce77617d8777
Jenn Lawson <jenniferlawson000@gmail.com>
2018-09-27 13:27:12 -0500

    edited view model

--- BangazonScrumptiousJellyfish/BangazonScrumptiousJellyfish/Models/EmployeeEditViewModel.cs
+++ BangazonScrumptiousJellyfish/BangazonScrumptiousJellyfish/Models/EmployeeEditViewModel.cs
@@ -62,7 +62,7 @@ namespace BangazonScrumptiousJellyfish.Models
             {
                 List<Department> department = (conn.Query<Department>(sql2)).ToList();
 
-                this.Computer = computer
+                this.Department = department
                     .Select(li => new SelectListItem
                     {
                         Text = li.DepartmentName,
