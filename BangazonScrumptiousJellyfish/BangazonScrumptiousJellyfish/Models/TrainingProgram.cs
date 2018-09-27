131a995197220c2841722a2ce5c7e100fc93f15b
element2562 <element25668@gmail.com>
2018-09-27 11:53:22 -0500

    Finished the Training program and Employee Training program intersection model.

---
+++ BangazonScrumptiousJellyfish/BangazonScrumptiousJellyfish/Models/TrainingProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonScrumptiousJellyfish.Models
+{
+    public class TrainingProgram
+    {
+
+        public int TrainingProgramId { get; set; }
+        [Required]
+        [Display (Name = "Program Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display (Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+        [Required]
+        [Display (Name = "End Date")]
+        public DateTime EndDate { get; set; }
+        [Required]
+        public int Capacity { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+}
