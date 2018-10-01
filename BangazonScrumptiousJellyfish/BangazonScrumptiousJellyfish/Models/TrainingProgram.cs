db7d83e9d903ecd9baee847033990e9610d0625c
sathvikreddy615 <sathvik.reddy615@gmail.com>
2018-10-01 15:39:41 -0500

    added Descrip field to Training Program's model, controller and views

--- BangazonScrumptiousJellyfish/BangazonScrumptiousJellyfish/Models/TrainingProgram.cs
+++ BangazonScrumptiousJellyfish/BangazonScrumptiousJellyfish/Models/TrainingProgram.cs
@@ -13,12 +13,21 @@ namespace BangazonScrumptiousJellyfish.Models
         [Required]
         [Display (Name = "Program Name")]
         public string ProgramName { get; set; }
+
+        [Required]
+        [Display (Name = "Description")]
+        public string Descrip { get; set; }
+
         [Required]
         [Display (Name = "Start Date")]
+        [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
+
         [Required]
         [Display (Name = "End Date")]
+        [DataType(DataType.Date)]
         public DateTime EndDate { get; set; }
+
         [Required]
         public int MaximumAttendees { get; set; }
         public List<Employee> Employees { get; set; }
