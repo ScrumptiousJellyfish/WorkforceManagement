dc1ad433a3975b4832b4b7878cbc25fe79e171a8
Jordan Williams <jordanwilliams288@gmail.com>
2018-10-01 09:55:38 -0500

    fixed merge conflicts in the view

--- BangazonScrumptiousJellyfish/BangazonScrumptiousJellyfish/Controllers/DepartmentController.cs
+++ BangazonScrumptiousJellyfish/BangazonScrumptiousJellyfish/Controllers/DepartmentController.cs
@@ -52,7 +52,7 @@ namespace BangazonScrumptiousJellyfish.Controllers
         }
 
 
-
+        //GET: Department/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
